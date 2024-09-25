1df3775501ef8cc08b048c2516d37dde5f5bcc72
Wonster <wooonster@outlook.com>
2024-09-26 03:15:13 +1000

    section1.1: build the NanoGPT model, with weights from HF pretrained models

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Inspired by and Learned from [Andrej Karpathy](https://www.youtube.com/watch?v=kCc8FmEb1nY&list=PLAqhIrjkxbuWI23v9cThsA9GvCAUhRvKZ&index=8).
 
 
+## GPT 2
+
+The gpt 2 this project reproduce is based on the 124M model size, with 12 layers of Transformers and 768 d_model size.
+
+The model is written using PyTorch, and Huggingface Transformers.
---
+++ nano_gpt/train_gpt2.py
@@ -0,0 +1,160 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import math
+from transformers import GPT2LMHeadModel
+
+
+# -------------------------------------------------------------------
+
+@dataclass
+class GPTConfigs:
+    block_size: int = 1024    # max sequence length
+    vocab_size: int = 50257   # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> special tokens
+    n_layer: int = 12         # number of layers
+    n_head: int = 12          # number of heads
+    n_embed: int = 768        # embedding dimensions
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc   = nn.Linear(config.n_embed, 4 * config.n_embed)
+        self.gelu   = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embed, config.n_embed)
+    
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+    
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embed % config.n_head == 0
+        # K, Q, V projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embed, 3 * config.n_embed)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embed, config.n_embed)
+        # regularization
+        self.n_head = config.n_head
+        self.n_embed = config.n_embed
+
+        # not actually a 'bias', more like a mask, just following gpt2's naming
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
+        
+    def forward(self, x):
+        B, T, C = x.size()  # B: batch_size, T: sequence length, C: embedding dimensionality (n_embed)
+        '''
+        calculate the query, keys, values for all heads in batch and move head forward to be the batch dim
+        nh: number of heads, hs: head size, C = nh * hs: number of channels
+        e.g. for gpt2(124M), n_head=12, head_size=64, C=n_head*head_size=768 channels in Transformer
+        '''
+        qkv = self.c_attn(x)
+        q, k ,v = qkv.split(self.n_embed, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+
+        # attention (materializes the large (T, T) matrix for all the queries and keys)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        # autoaggressive mask to make sure that the tokens only attend to tokens before
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C)  # re-assemble all head outputs side by side
+        # output projection
+        y = self.c_proj(y)
+        return y
+
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embed)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embed)
+        self.mlp = MLP(config)
+
+    '''
+    the layer norms are after the application of attention or feed forward
+    and they are inside the residual stream
+    '''
+    
+    def foward(self, x):
+        " the attentions function is where the tokens communicate, it's a aggragation function "
+        x += self.attn(self.ln_1(x))
+
+        " in the mlp, which happends every single token individually, no info being collected or exchanging "
+        x += self.mlp(self.ln_2(x))  # += : residual
+        return x
+
+class NanoGPT(nn.Module):
+    def __init__(self, config) -> None:
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embed),  # weights of token embed
+            wpe = nn.Embedding(config.block_size, config.n_embed),  # weights of positional embed
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),  # hidden layer
+            ln_f = nn.LayerNorm(config.n_embed),  # an additional final layer norm according to the paper
+        ))
+
+        # the final classifier, the language model head, projects the n_embed_dims to vocab_size
+        self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+
+    " load params from huggingface transformers "
+    @classmethod
+    def from_pretrained(cls, model_type):
+        " Load pretrained GPT-2 model weights from huggingface "
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        print(f"Loading weights from pretrained gpt of type: {model_type}")
+
+        # n_layer, n_head, n_embed are determined from model_type
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embed=768),  # 124M params
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embed=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embed=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embed=1600), # 1558M params
+        }[model_type]
+        config_args['vocab_size'] = 50257
+        config_args['block_size'] = 1024
+        # create a form-scratch initialized nanoGPT model
+        config = GPTConfigs(**config_args)
+        model = NanoGPT(config=config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]  # discard this mask / buffer, not a param
+
+        # init a huggingface/transformer model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the params are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')]  # ignore the 2 buffers
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')]
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+    
+# ------------------------------------------------------------------
+model = NanoGPT.from_pretrained('gpt2')
+print('worked')
