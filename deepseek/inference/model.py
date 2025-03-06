c19ee07c025cda1c257e78b467fb1f39dd672dfe
wonster <wooonster@outlook.com>
2025-03-06 11:29:14 +0800

    add transformer

--- deepseek/inference/model.py
+++ deepseek/inference/model.py
@@ -376,7 +376,7 @@ class MLA(nn.Module):
         softmax_scale: scaling factor for softmax in attention computation
     """
 
-    def __init__(self, args: ModelArgs0:
+    def __init__(self, args: ModelArgs):
         super().__init__()
         self.dim = args.dim
         self.n_heads = args.n_heads
@@ -634,4 +634,96 @@ class MoE(nn.Module):
         return (y + z).view(shape)
 
 
+class Block(nn.Module):
+    """
+    Transformer Block with attention and FFN.
+
+    Attributes, all nn.Module:
+        attn: MLA 
+        ffn: FFN (MLP or MoE)
+        attn_norm: layer norm for attention layer
+        ffn_norm: layer norm for FFN
+    """
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.attn = MLA(args)
+        self.ffn = MLP(args.dim, args.inter_dim) if layer_id < args.n_dense_layers else MoE(args)
+        self.attn_norm = RMSNorm(args.dim)
+        self.ffn_norm = RMSNorm(args.dim)
+
+    def forward(self, x: torch.Tensor, start_pos: int, freqs_cis: torch.Tensor, mask: Optional[torch.Tensor]) -> torch.Tensor:
+        """
+        Forward pass for Transformer block.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+            start_pos (int): Starting position in the sequence.
+            freqs_cis (torch.Tensor): Precomputed complex exponential values for rotary embeddings.
+            mask (Optional[torch.Tensor]): Mask tensor to exclude certain positions from attention.
+
+        Returns:
+            torch.Tensor: Output tensor after block computation.
+        """
+        # RMSNorm -> Attention -> ResConn
+        x = x + self.attn(self.attn_norm(x), start_pos, freqs_cis, mask)
+        # RMSNorm -> FFN -> ResConn
+        x = x + self.ffn(self.ffn_norm(x))
+        return
+
+class Transformer(nn.Module):
+    """
+    Transformer in Deepseek, with positional embedding, multiple layers, and output projection.
     
+    Attributes:
+        max_seq_len (int): Maximum sequence length for the transformer.
+        embed (nn.Module): Embedding layer for input tokens.
+        layers (torch.nn.ModuleList): List of transformer blocks.
+        norm (nn.Module): Layer normalization applied after all blocks.
+        head (nn.Module): Output projection layer mapping to vocabulary size.
+        freqs_cis (torch.Tensor): Precomputed complex exponential values for rotary embeddings.
+    """
+    def __init__(self, args: ModelArgs):
+        # 初始化 分布式变量
+        global world_size, rank
+        world_size = dist.get_world_size() if dist.is_initialized() else 1
+        rank = dist.get_rank() if dist.is_initialized() else 1
+        Linear.dtype = torch.float8_e4m3fn if args.dtype == 'fp8' else torch.bfloat16
+        
+        super().__init__()
+        self.max_seq_len = args.max_seq_len
+        self.embed = ParallelEmbedding(args.vocab_size, args.dim)
+        self.layers = torch.nn.ModuleList()
+        for layer_id in range(args.n_layers):
+            self.layers.append(Block(layer_id, args))
+        self.norm = RMSNorm(args.dim)
+        self.head = ColumnParallelLinear(args.dim, args.vocab_size, dtype=torch.get_default_dtype())
+        self.register_buffer("freqs_cis", precompute_freqs_cis(args), presistent=False)
+
+    @torch.inference_mode()
+    def forward(self, tokens: torch.Tensor, start_pos: int = 0):
+        """
+        Forward pass for the Transformer model.
+
+        Args:
+            tokens (torch.Tensor): Input tensor of token IDs with shape (batch_size, seq_len).
+            start_pos (int, optional): Starting position in the sequence for rotary embeddings. Defaults to 0.
+
+        Returns:
+            torch.Tensor: Logits tensor of shape (batch_size, vocab_size).
+        """
+        seqlen = tokens.size(1)
+        h = self.embed(tokens)  # get hidden state
+        freqs_cis = self.freqs_cis[start_pos:start_pos + seqlen]
+        mask = None
+        if seqlen > 1:
+            mask = torch.full((seqlen, seqlen), float("-inf"), device=tokens.device).triu_(1)
+        for layer in self.layers:
+            h = layer(h, start_pos, freqs_cis, mask)
+        h = self.norm(h)[:, -1]  # 归一化
+        logits = self.head(h)    # 计算 logits
+        if world_size > 1:
+            all_logits = [torch.empty_like(logits) for _ in range(world_size)]
+            dist.all_gather(all_logits, logits)
+            # 合并所有 logits
+            logits = torch.cat(all_logits, dim=-1)
+        return logits
